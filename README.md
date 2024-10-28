36300c86b1cf3882e583a848cdcfe8a77044ba5d
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2024-10-27 20:41:12 -0400

    updated comments

--- README.md
+++ README.md
@@ -19,8 +19,8 @@
 
 1. **Configure the script:**
 
-   - Set `link`, `date`, `actTime`, `actName`, and `numberOfPeople` for your reservation.
-   - Set your `number`, `email`, and `name` for the booking.
+   - Set `link`, `date`, `actTime`, `actName`, and `numberOfPeople` for your reservation in `activityInfo.py`.
+   - Set your `number`, `email`, and `name` for the booking in `activityInfo.py`.
 
 2. **Run the script:**
    - The script starts refreshing the page at 6:00 PM EST until it finds and books the desired slot.
--- activityInfo.py
+++ activityInfo.py
@@ -3,11 +3,12 @@
 
 # Activity Details
 daysOffset = 2 # Number of days from today to the desired reservation date. Default is 2 days
-# cardelrec
-link = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
 
-# richcraftrec
-# link = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+cardelrec = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+richcraftrec = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+
+# Link to the activity reservation page of the desired facility
+link = cardelrec
 
 # Activity Details
 actName = "Badminton - 16+"
@@ -15,6 +16,6 @@ actTime = "10:00 AM"
 numberOfPeople = '2'
 
 # Personal Detail
-# number = "6139995555"
-# email = "example@gmail.com"
-# name = "First Last"
+number = "6139995555"
+email = "example@gmail.com"
+name = "First Last"
