6108485f2e354ac535ede21434c269fff46f1d95
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2024-10-27 20:37:51 -0400

    updated readme

--- activityInfo.py
+++ activityInfo.py
@@ -2,20 +2,19 @@
 # Make sure the data, activity name, and activity time are exactly as they appear on the website
 
 # Activity Details
-# x days from today
-daysOffset = 2 
+daysOffset = 2 # Number of days from today to the desired reservation date. Default is 2 days
 # cardelrec
-# link = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
-# richcraftrec
-link = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+link = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
 
-# actName = "Aqua general - deep"
-actName = "Badminton doubles - all ages"
-# actName = "Volleyball - Adult"
-actTime = "7:00 PM"
+# richcraftrec
+# link = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+
+# Activity Details
+actName = "Badminton - 16+"
+actTime = "10:00 AM"
 numberOfPeople = '2'
 
 # Personal Detail
-number = "6139815014"
-email = "inbox.kpatel@gmail.com"
-name = "Karan Patel"
+# number = "6139995555"
+# email = "example@gmail.com"
+# name = "First Last"
