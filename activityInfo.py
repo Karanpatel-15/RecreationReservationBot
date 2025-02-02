6d2c2a751b8b9ee48265f4982f24c91f5afeeaec
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2025-02-02 18:14:30 -0500

    added date offset

--- activityInfo.py
+++ activityInfo.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 # Replace the following variables with the correct values for your reservation
 # Make sure the data, activity name, and activity time are exactly as they appear on the website
+
+# Activity Details Presets
 cardelrec = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
 richcraftrec = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
 
-# Activity Details Presets
+DAYS_OFFSET = 0 # Number of days from today to the desired reservation date. Default is 2 days
+REFRESH_TIME = datetime.now() + timedelta(seconds=10)
+# datetime.now().replace(hour=18, minute=0, second=0, microsecond=0)
 
 richcraftBadmintonTue = {
     "link": richcraftrec,
