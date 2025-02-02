c1fd4a876031363de1a50d3027295749e2ba46e6
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2025-02-02 18:13:30 -0500

    added link var

--- activityInfo.py
+++ activityInfo.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 # Replace the following variables with the correct values for your reservation
 # Make sure the data, activity name, and activity time are exactly as they appear on the website
+cardelrec = "https://reservation.frontdesksuite.ca/rcfs/cardelrec/Home/Index?Culture=en&PageId=a10d1358-60a7-46b6-b5e9-5b990594b108&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
+richcraftrec = "https://reservation.frontdesksuite.ca/rcfs/richcraftkanata/Home/Index?Culture=en&PageId=b3b9b36f-8401-466d-b4c4-19eb5547b43a&ShouldStartReserveTimeFlow=False&ButtonId=00000000-0000-0000-0000-000000000000"
 
 # Activity Details Presets
 
